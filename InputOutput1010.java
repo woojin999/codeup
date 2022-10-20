1a0b75da60f314b345c2e8ef67348d51f48b093d
woojin999 <24864@naver.com>
2022-10-21 01:03:07 +0900

    Create InputOutput1010.java

---
+++ InputOutput1010.java
@@ -0,0 +1,13 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1010 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+	}
+
+}
