e8f67a94fdc2dac5c292c82ea5b309e874bf97ae
LEE WOOJIN <24864@naver.com>
2022-10-23 00:39:37 +0900

    Update InputOutput1012.java

--- InputOutput1012.java
+++ InputOutput1012.java
@@ -6,6 +6,6 @@ public class InputOutput1012 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		float f = sc.nextFloat();
-		System.out.printf("%.5f", f);
+		System.out.printf("%.6f", f);
 	}
 }
