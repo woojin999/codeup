64b1ab24c4f25affc92ca0ae70487c23e02cbad8
woojin999 <24864@naver.com>
2022-10-25 20:00:59 +0900

    InputOutput

---
+++ InputOutput/InputOutput1014.java
@@ -0,0 +1,12 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1014 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		char b = sc.next().charAt(0);
+		System.out.println(b+" "+a);
+	}
+}
