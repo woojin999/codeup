9606ac2d4fd7ef9288c0927820ffc2de081b1acd
woojin999 <24864@naver.com>
2022-10-26 00:35:15 +0900

    Create InputOutput1015.java

---
+++ InputOutput/InputOutput1015.java
@@ -0,0 +1,11 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1015 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float f = sc.nextFloat();
+		System.out.printf("%.2f",f);
+	}
+}
