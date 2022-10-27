58b7eac374f0a082289842ee12d9c8dc5b329f41
woojin999 <24864@naver.com>
2022-10-27 16:58:56 +0900

    Create InputOutput1016.java

---
+++ InputOutput/InputOutput1016.java
@@ -0,0 +1,11 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1016 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.printf("%d %d %d",n, n ,n);
+	}
+}
