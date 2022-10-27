917a15cbf66fb52697b97456c7d819d56316ea84
woojin999 <24864@naver.com>
2022-10-28 00:18:49 +0900

    Create InputOutput1017.java

---
+++ InputOutput/InputOutput1017.java
@@ -0,0 +1,15 @@
+package InputOutput;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class InputOutput1017 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.printf("%d %d %d",n, n ,n);
+//		for (int i = 0; i < 3; i++) {
+//			System.out.printf("%d ",n);
+//		}
+	}
+}
