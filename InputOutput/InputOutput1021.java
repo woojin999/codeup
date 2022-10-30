a4febb6fe6fea98e636c4ef188337dfbb1484c69
woojin999 <24864@naver.com>
2022-10-30 17:25:21 +0900

    Create InputOutput1021.java

---
+++ InputOutput/InputOutput1021.java
@@ -0,0 +1,11 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1021 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String t = sc.next();
+		System.out.printf("%s", t);
+	}
+}
