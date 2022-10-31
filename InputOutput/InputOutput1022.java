268344369b33a6855b2b185b361150c61f02001b
woojin999 <24864@naver.com>
2022-10-31 14:16:06 +0900

    Create InputOutput1022.java

---
+++ InputOutput/InputOutput1022.java
@@ -0,0 +1,11 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutput1022 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String t = sc.nextLine();
+		System.out.println(t);
+	}
+}
