7aa07d6853ac30f6e60bd45a1593530edbc4ef67
woojin999 <24864@naver.com>
2022-11-03 14:51:04 +0900

    Create DataType1028.java

---
+++ DataType/DataType1028.java
@@ -0,0 +1,18 @@
+package DataType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DataType1028 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long num = Long.parseLong(br.readLine());
+		System.out.print(num);
+//		Scanner sc = new Scanner(System.in);
+//		long n = sc.nextLong();
+//		System.out.printf("%d", n);
+	}
+
+}
