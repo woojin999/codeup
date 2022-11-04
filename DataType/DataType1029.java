894ae017526adc839619cb404d94dedccdc93af3
woojin999 <24864@naver.com>
2022-11-04 15:57:36 +0900

    Create DataType1029.java

---
+++ DataType/DataType1029.java
@@ -0,0 +1,13 @@
+package DataType;
+
+import java.util.Scanner;
+
+public class DataType1029 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		System.out.printf(String.format("%.11f", n));
+	}
+
+}
