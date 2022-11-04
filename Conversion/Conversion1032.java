971ff2e1c2bbe12e8a9401718f0ef64b2bb24e84
woojin999 <24864@naver.com>
2022-11-05 00:08:20 +0900

    Create Conversion1032.java

---
+++ Conversion/Conversion1032.java
@@ -0,0 +1,13 @@
+package Conversion;
+
+import java.util.Scanner;
+
+public class Conversion1032 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.printf("%x",n);
+	}
+
+}
