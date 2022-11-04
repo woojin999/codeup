530afd2c97a3daccc8f9813b563bdeb081c5686f
woojin999 <24864@naver.com>
2022-11-04 15:57:38 +0900

    Create DataType1030.java

---
+++ DataType/DataType1030.java
@@ -0,0 +1,13 @@
+package DataType;
+
+import java.util.Scanner;
+
+public class DataType1030 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.printf("%d",n);
+	}
+
+}
