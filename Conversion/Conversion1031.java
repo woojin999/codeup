37fbf896f67c0536478df6cbbdcabf7964ca3608
woojin999 <24864@naver.com>
2022-11-05 00:11:28 +0900

    Update Conversion1031.java

--- Conversion/Conversion1031.java
+++ Conversion/Conversion1031.java
@@ -7,7 +7,7 @@ public class Conversion1031 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long n = sc.nextLong();
-		System.out.printf("%X",n);
+		System.out.printf("%o",n);
 	}
 
 }
