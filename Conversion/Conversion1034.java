c205732de468da996b41f71780b2f167da996d17
woojin999 <24864@naver.com>
2022-11-06 22:00:04 +0900

    Create Conversion1034.java

---
+++ Conversion/Conversion1034.java
@@ -0,0 +1,17 @@
+package Conversion;
+
+import java.util.Scanner;
+
+public class Conversion1034 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		int num = Integer.parseInt(n, 8);
+		System.out.print(num);
+	}
+
+}
+
+
+
