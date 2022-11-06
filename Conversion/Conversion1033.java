58a2acf05e6b73b36f7c011925291150edd0f3a0
woojin999 <24864@naver.com>
2022-11-06 22:00:02 +0900

    Create Conversion1033.java

---
+++ Conversion/Conversion1033.java
@@ -0,0 +1,16 @@
+package Conversion;
+
+import java.util.Scanner;
+
+public class Conversion1033 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.printf("%X",n);
+	}
+
+}
+
+
+
