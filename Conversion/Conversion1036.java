26726838967a44a5dfa04161a571ae9fd4bc1057
woojin999 <24864@naver.com>
2022-11-07 16:44:53 +0900

    Create Conversion1036.java

---
+++ Conversion/Conversion1036.java
@@ -0,0 +1,16 @@
+package Conversion;
+
+import java.util.Scanner;
+
+public class Conversion1036 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char n = sc.next().charAt(0);
+		System.out.print((int)n);
+	}
+
+}
+
+
+
