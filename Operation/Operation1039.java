fdfc0a13fd51119e2503429e78c4814669651197
woojin999 <24864@naver.com>
2022-11-09 00:07:08 +0900

    Create Operation1039.java

---
+++ Operation/Operation1039.java
@@ -0,0 +1,14 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1039 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n1 = sc.nextLong();
+		long n2 = sc.nextLong();
+		System.out.println(n1 + n2);
+	}
+
+}
