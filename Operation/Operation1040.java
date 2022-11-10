5f6d87c01406f56a8cc95d61fdf2d79613f31fd2
woojin999 <24864@naver.com>
2022-11-10 18:01:49 +0900

    Create Operation1040.java

---
+++ Operation/Operation1040.java
@@ -0,0 +1,13 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1040 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println(-n);
+	}
+
+}
