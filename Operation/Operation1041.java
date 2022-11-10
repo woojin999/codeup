71f0fd238ced91f13788b9324c52871cb073a65f
woojin999 <24864@naver.com>
2022-11-10 18:01:50 +0900

    Create Operation1041.java

---
+++ Operation/Operation1041.java
@@ -0,0 +1,14 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1041 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		int b = (int)a+1;
+		System.out.println((char)b);
+	}
+
+}
