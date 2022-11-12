7f78e8a1142072178745c5ba5c15a40d69fad7c5
woojin999 <24864@naver.com>
2022-11-13 00:37:26 +0900

    Create Operation1044.java

---
+++ Operation/Operation1044.java
@@ -0,0 +1,13 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1044 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		a++;
+		System.out.printf("%d",a);
+	}
+}
