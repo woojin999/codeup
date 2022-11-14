60cfef6e1721060bb3a87f5ca5278c8dde08278e
woojin999 <24864@naver.com>
2022-11-14 11:20:07 +0900

    Create Operation1045.java

---
+++ Operation/Operation1045.java
@@ -0,0 +1,22 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1045 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		float b = sc.nextFloat();
+		System.out.println((long)(a+b));
+		System.out.println((long)(a+b));
+		System.out.println((long)(a+b));
+		System.out.println((long)(a+b));
+		System.out.println((long)(a+b));
+		float c = a / b;
+		System.out.printf("%.2f", c);
+		
+		
+		
+	}
+}
