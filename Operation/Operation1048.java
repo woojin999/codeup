2aff857c69690ba900bd2e00abee1fa6a8c3ef13
woojin999 <24864@naver.com>
2022-11-15 22:55:21 +0900

    Create Operation1048.java

---
+++ Operation/Operation1048.java
@@ -0,0 +1,15 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1048 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long x = a<<b;
+		System.out.println(x);
+		
+	}
+}
