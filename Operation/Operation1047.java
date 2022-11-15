6eb59a4da47c01572a22a21ec7fc0cc9c85ac00b
woojin999 <24864@naver.com>
2022-11-15 22:55:19 +0900

    Create Operation1047.java

---
+++ Operation/Operation1047.java
@@ -0,0 +1,14 @@
+package Operation;
+
+import java.util.Scanner;
+
+public class Operation1047 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long x = a*2;
+		System.out.println(x);
+		
+	}
+}
