ea431a26e4c9847ba4cc78ed63e8acfe7e83ff69
woojin999 <24864@naver.com>
2022-12-03 16:34:45 +0900

    Create Conditional1066.java

---
+++ Conditional/Conditional1066.java
@@ -0,0 +1,34 @@
+package Conditional;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Conditional1066 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long a = Long.parseLong(br.readLine());
+		long b = Long.parseLong(br.readLine());
+		long c = Long.parseLong(br.readLine());
+		
+		if (a % 2 == 0) {
+			System.out.println("even");
+		} else  {
+			System.out.println("odd");
+		}
+		if (b % 2 == 0) {
+			System.out.println("even");
+		}else  {
+			System.out.println("odd");
+		}
+		if (c % 2 == 0) {
+			System.out.println("even");
+		}else  {
+			System.out.println("odd");
+		}
+		
+	}
+}
