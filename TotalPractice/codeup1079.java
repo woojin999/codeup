e41d546b38da792895df702e77ce18c822613cb8
woojin999 <24864@naver.com>
2022-12-22 15:49:39 +0900

    Create codeup1079.java

---
+++ TotalPractice/codeup1079.java
@@ -0,0 +1,20 @@
+package TotalPratice;
+
+import java.util.Scanner;
+
+public class codeup1079 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			char data = sc.next().charAt(0);
+			if (data != 'q') {
+				System.out.println(data);
+			} else {
+				System.out.println(data);
+				break;
+			}
+			
+		}
+	}
+}
